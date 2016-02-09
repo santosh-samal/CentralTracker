78da381bc19771fb7a7d86d988cf20c7406f3a25
sagarsakti1986 <sagarsakti.swain@gmail.com>
2016-02-09 19:18:38 +0530

    Added new page

---
+++ Sagar/MyPersonalWork/MyPersonalWork/Mytest.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MyPersonalWork
+{
+    public partial class Mytest : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
